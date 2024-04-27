e78d4e7ae01ddc15e2e582cec019f18c283f5830
willmaster15 <168244086+willmaster15@users.noreply.github.com>
2024-04-26 23:57:11 -0500

    Update servycomApp.js

--- servycomApp.js
+++ servycomApp.js
@@ -5,7 +5,7 @@ const uuid = require('uuid');
 const moment = require('moment');
 
 const app = express();
-const port = 21;
+const port = process.env.PORT || 3306;
 
 // Configuración de la conexión a la base de datos MySQL
 const connection = mysql.createConnection({
